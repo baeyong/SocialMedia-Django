6d93b2e69742cd09982bf16b59a1dc9906a9371e
Taeyong Lee <taeyongplee@gmail.com>
2023-05-24 17:17:24 -0400

    adding more allowed hosts

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.31.7.25']
+ALLOWED_HOSTS = ['172.31.7.25', '127.0.0.1']
 
 
 # Application definition
