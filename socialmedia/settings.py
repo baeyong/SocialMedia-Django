9a976ef1e2fc610fb3a0b1bef8e7aed4b9804488
Taeyong Lee <taeyongplee@gmail.com>
2023-05-25 15:29:54 -0400

    getting rid of decouple module

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+# from decouple import config
 from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
