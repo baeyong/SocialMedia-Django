e55b92f9c9c7699418cd827e9ef42a04b34405f8
Taeyong Lee <taeyongplee@gmail.com>
2023-05-28 22:56:54 -0400

    mysql

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "socialmedia.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": 'django.db.backends.sqlite3',
+        "ENGINE": 'django.db.backends.mysql',
         "NAME": 'django',
         "USER": '',
         "PASSWORD": '',
