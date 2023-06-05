9454023f05769a4e5207eb9ed2d29c0b282efb46
Taeyong Lee <taeyongplee@gmail.com>
2023-06-05 14:18:24 -0400

    ip

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.17.57.255', '127.0.0.1']
+ALLOWED_HOSTS = ['52.14.69.196', '127.0.0.1']
 
 
 # Application definition
