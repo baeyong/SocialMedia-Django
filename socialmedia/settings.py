d6b6b978577e6fe5cdf266feb1e4942185d12682
Taeyong Lee <taeyongplee@gmail.com>
2023-06-08 11:43:12 -0400

    default settings

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+# from decouple import config
 from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,5 +137,9 @@ MEDIA_ROOT = BASE_DIR / 'socialnetwork/media'
 
 MEDIA_URL = 'media/'
 
+# Trying to fix the log out issue
+SESSION_COOKIE_SECURE = False
+
+SESSION_COOKIE_AGE = 86400
 
 
